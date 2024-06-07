3990b201e38f9bb356ee90fbc2e171e15907ff35
toyoubala <TOYOUBALA@GMAIL.COM>
2024-06-07 10:56:01 -0400

    Squash and commit (#2)
    * s1
    
    * s2
    
    * -s3
    
    ---------
    
    Co-authored-by: Balamurugan Subramanian <balams@cisco.com>

--- userinfo.c
+++ userinfo.c
@@ -12,6 +12,19 @@
 
 **************************************/
 
+/**************************************
 void getuserinfo()
+ File  : userinfo.c
+
+ Author: balams
+
+ Date  : 2024-06-07
+
+ Copyright (c) 2024 by Cisco Systems, Inc.
+
+ All rights reserved.
+
+**************************************/
+
 {
 }

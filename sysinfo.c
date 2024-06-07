f337e9c86e39b9c5ad8b3a80d30901062367afac
Balamurugan Subramanian <balams@cisco.com>
2024-06-07 10:52:10 -0400

    -s3

--- sysinfo.c
+++ sysinfo.c
@@ -12,6 +12,19 @@
 
 **************************************/
 
+/**************************************
 void getsysinf()
+ File  : sysinfo.c
+
+ Author: balams
+
+ Date  : 2024-06-07
+
+ Copyright (c) 2024 by Cisco Systems, Inc.
+
+ All rights reserved.
+
+**************************************/
+
 {
 }
